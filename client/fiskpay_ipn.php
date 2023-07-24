9d08bd7ab00aae76b0d75ecc3cb7d4f1aeaf61ff
Scrabakos <scrab4ever@hotmail.com>
2023-07-24 22:58:39 +0300

    added commends

--- client/fiskpay_ipn.php
+++ client/fiskpay_ipn.php
@@ -25,28 +25,28 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && ($iPNData = @file_get_contents('php
 
 			if ($responseObject->error === false && ($orderObject = $responseObject->data->order)) {
 
-				$network = $orderObject->network; // The network that the transaction took place
-				$timestamp = $orderObject->timestamp; // The timestamp that this object was created
-				$verification = $orderObject->verification; // The transaction verification
+				$network = $orderObject->network; 												// variable type: string - The network that the transaction took place
+				$timestamp = $orderObject->timestamp; 											// variable type: string - The timestamp that this object was created
+				$verification = $orderObject->verification; 									// variable type: string - The transaction verification
 
-				$txHash = $orderObject->txHash; // The blockchain transaction hash
-				$sender = $orderObject->sender; // The address where the funds are sent from
-				$receiver = $orderObject->receiver; // The address where the funds are sent to. Should be equal to your wallet address ($wallet)
+				$txHash = $orderObject->txHash; 												// variable type: string - The blockchain transaction hash
+				$sender = $orderObject->sender; 												// variable type: string - The address where the funds are sent from
+				$receiver = $orderObject->receiver; 											// variable type: string - The address where the funds are sent to. Should be equal to your wallet address ($wallet)
 
-				$cryptoCurrencySymbol = $orderObject->cryptoCurrency->symbol; // The symbol of the crypto currency sent
-				$cryptoCurrencyAmount = $orderObject->cryptoCurrency->amount; // The amount of the crypto currency sent
-				$cryptoCurrencyTotalUSDValue = $orderObject->cryptoCurrency->totalUSDValue; // The total value of the crypto currency sent in USD
-				$cryptoCurrencyUnitUSDValue = $orderObject->cryptoCurrency->unitUSDValue; // The value of 1 crypto currency in USD
+				$cryptoCurrencySymbol = $orderObject->cryptoCurrency->symbol; 					// variable type: string - The symbol of the cryptocurrency sent
+				$cryptoCurrencyAmount = $orderObject->cryptoCurrency->amount; 					// variable type: string - The amount of the cryptocurrency sent
+				$cryptoCurrencyTotalUSDValue = $orderObject->cryptoCurrency->totalUSDValue; 	// variable type: string - The total value of the cryptocurrency sent in USD
+				$cryptoCurrencyUnitUSDValue = $orderObject->cryptoCurrency->unitUSDValue; 		// variable type: string - The value of 1 cryptocurrency in USD
 
-				$fiatCurrencySymbol = $orderObject->fiatCurrency->symbol; // The symbol of the fiat currency that was used for the fiat to crypto conversion
-				$fiatCurrencyAmount = $orderObject->fiatCurrency->amount; // The amount of fiat currency used for the converstion
-				$fiatCurrencyTotalUSDValue = $orderObject->fiatCurrency->totalUSDValue; // The total value of the fiat currency used for the converstion in USD
-				$fiatCurrencyUnitUSDValue = $orderObject->fiatCurrency->unitUSDValue; // The value of 1 fiat currency in USD
+				$fiatCurrencySymbol = $orderObject->fiatCurrency->symbol; 						// variable type: string - The symbol of the fiat currency that was used for the fiat to crypto conversion
+				$fiatCurrencyAmount = $orderObject->fiatCurrency->amount; 						// variable type: string - The amount of fiat currency used for the converstion
+				$fiatCurrencyTotalUSDValue = $orderObject->fiatCurrency->totalUSDValue; 		// variable type: string - The total value of the fiat currency used for the converstion in USD
+				$fiatCurrencyUnitUSDValue = $orderObject->fiatCurrency->unitUSDValue;			// variable type: string - The value of 1 fiat currency in USD
 
-				$postDataItem1 = $orderObject->postData->item1; // The first callback item
-				$postDataItem2 = $orderObject->postData->item2; // The second callback item
-				$postDataItem3 = $orderObject->postData->item3; // The third callback item
-				$postDataItem4 = $orderObject->postData->item4; // The forth callback item
+				$postDataItem1 = $orderObject->postData->item1;									// variable type: any - The first callback item
+				$postDataItem2 = $orderObject->postData->item2;									// variable type: any - The second callback item
+				$postDataItem3 = $orderObject->postData->item3;									// variable type: any - The third callback item
+				$postDataItem4 = $orderObject->postData->item4;									// variable type: any - The forth callback item
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //------------------------------------------------------------ DO NOT EDIT THE CODE ABOVE THIS COMMENT ------------------------------------------------------------//
