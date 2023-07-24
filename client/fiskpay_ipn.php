7fe6401e65d0b3faed874b8d90482141b938c401
Scrabakos <scrab4ever@hotmail.com>
2023-07-24 23:10:21 +0300

    change logging folder

--- client/fiskpay_ipn.php
+++ client/fiskpay_ipn.php
@@ -10,45 +10,45 @@
 //Add your wallet address in the $wallet parameter.
 $wallet = "0x41dA7A1e5085179F43758dC5F0a5bBEB012E07F1";
 
-//Set $logTransactions parameter as true, to create a json file on transaction received.
+//Set $logTransactions parameter as true, to create a json file on every new transaction that you receive.
 $logTransactions = true;
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //------------------------------------------------------------ DO NOT EDIT THE CODE BELOW THIS COMMENT ------------------------------------------------------------//
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
-if ($_SERVER['REQUEST_METHOD'] === "POST" && ($iPNData = @file_get_contents('php://input'))) {
+if ($_SERVER["REQUEST_METHOD"] === "POST" && ($iPNData = @file_get_contents("php://input"))) {
 
 	if (preg_match("/^[a-zA-Z0-9\/\r\n+]*={0,2}$/", $iPNData) && $responseString = @file_get_contents("https://app.fiskpay.com/claimOrder/" . $iPNData . "/" . $wallet)) {
 
-		if (($responseObject = json_decode($responseString)) !== null && property_exists($responseObject, 'error') && property_exists($responseObject, 'message') && property_exists($responseObject, 'data')) {
+		if (($responseObject = json_decode($responseString)) !== null && property_exists($responseObject, "error") && property_exists($responseObject, "message") && property_exists($responseObject, "data")) {
 
 			if ($responseObject->error === false && ($orderObject = $responseObject->data->order)) {
 
-				$network = $orderObject->network; 												// variable type: string - The network that the transaction took place
-				$timestamp = $orderObject->timestamp; 											// variable type: string - The timestamp that this object was created
-				$verification = $orderObject->verification; 									// variable type: string - The transaction verification
+				$network = $orderObject->network; // variable type: string - The network that the transaction took place
+				$timestamp = $orderObject->timestamp; // variable type: string - The timestamp that this object was created
+				$verification = $orderObject->verification; // variable type: string - The transaction verification
 
-				$txHash = $orderObject->txHash; 												// variable type: string - The blockchain transaction hash
-				$sender = $orderObject->sender; 												// variable type: string - The address where the funds are sent from
-				$receiver = $orderObject->receiver; 											// variable type: string - The address where the funds are sent to. Should be equal to your wallet address ($wallet)
+				$txHash = $orderObject->txHash; // variable type: string - The blockchain transaction hash
+				$sender = $orderObject->sender; // variable type: string - The address where the funds are sent from
+				$receiver = $orderObject->receiver; // variable type: string - The address where the funds are sent to. Should be equal to your wallet address ($wallet)
 
-				$cryptoCurrencySymbol = $orderObject->cryptoCurrency->symbol; 					// variable type: string - The symbol of the cryptocurrency sent
-				$cryptoCurrencyAmount = $orderObject->cryptoCurrency->amount; 					// variable type: string - The amount of the cryptocurrency sent
-				$cryptoCurrencyTotalUSDValue = $orderObject->cryptoCurrency->totalUSDValue; 	// variable type: string - The total value of the cryptocurrency sent in USD
-				$cryptoCurrencyUnitUSDValue = $orderObject->cryptoCurrency->unitUSDValue; 		// variable type: string - The value of 1 cryptocurrency in USD
+				$cryptoCurrencySymbol = $orderObject->cryptoCurrency->symbol; // variable type: string - The symbol of the cryptocurrency sent
+				$cryptoCurrencyAmount = $orderObject->cryptoCurrency->amount; // variable type: string - The amount of the cryptocurrency sent
+				$cryptoCurrencyTotalUSDValue = $orderObject->cryptoCurrency->totalUSDValue; // variable type: string - The total value of the cryptocurrency sent in USD
+				$cryptoCurrencyUnitUSDValue = $orderObject->cryptoCurrency->unitUSDValue; // variable type: string - The value of 1 cryptocurrency in USD
 
-				$fiatCurrencySymbol = $orderObject->fiatCurrency->symbol; 						// variable type: string - The symbol of the fiat currency that was used for the fiat to crypto conversion
-				$fiatCurrencyAmount = $orderObject->fiatCurrency->amount; 						// variable type: string - The amount of fiat currency used for the converstion
-				$fiatCurrencyTotalUSDValue = $orderObject->fiatCurrency->totalUSDValue; 		// variable type: string - The total value of the fiat currency used for the converstion in USD
-				$fiatCurrencyUnitUSDValue = $orderObject->fiatCurrency->unitUSDValue;			// variable type: string - The value of 1 fiat currency in USD
+				$fiatCurrencySymbol = $orderObject->fiatCurrency->symbol; // variable type: string - The symbol of the fiat currency that was used for the fiat to crypto conversion
+				$fiatCurrencyAmount = $orderObject->fiatCurrency->amount; // variable type: string - The amount of fiat currency used for the converstion
+				$fiatCurrencyTotalUSDValue = $orderObject->fiatCurrency->totalUSDValue; // variable type: string - The total value of the fiat currency used for the converstion in USD
+				$fiatCurrencyUnitUSDValue = $orderObject->fiatCurrency->unitUSDValue; // variable type: string - The value of 1 fiat currency in USD
 
-				$postDataItem1 = $orderObject->postData->item1;									// variable type: any - The first callback item
-				$postDataItem2 = $orderObject->postData->item2;									// variable type: any - The second callback item
-				$postDataItem3 = $orderObject->postData->item3;									// variable type: any - The third callback item
-				$postDataItem4 = $orderObject->postData->item4;									// variable type: any - The forth callback item
+				$postDataItem1 = $orderObject->postData->item1; // variable type: any - The first callback item
+				$postDataItem2 = $orderObject->postData->item2; // variable type: any - The second callback item
+				$postDataItem3 = $orderObject->postData->item3; // variable type: any - The third callback item
+				$postDataItem4 = $orderObject->postData->item4; // variable type: any - The forth callback item
 
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------//
+				//-----------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //------------------------------------------------------------ DO NOT EDIT THE CODE ABOVE THIS COMMENT ------------------------------------------------------------//
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
@@ -78,18 +78,18 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && ($iPNData = @file_get_contents('php
 
 				}
 
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------//
+				//-----------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //------------------------------------------------------------ DO NOT EDIT THE CODE BELOW THIS COMMENT ------------------------------------------------------------//
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 				if ($logTransactions == true) {
 
-					$orderDir = "./FiskPayOrders/";
+					$orderDir = "./FiskPayOrders/" + date("Y-m-d", $timestamp) + "/" + $network + "/";
 
 					if (is_dir($orderDir) == false)
 						mkdir($orderDir, 0750, true);
 
-					$orderHandler = fopen(($orderDir . $network . "_" . $verification . ".json"), 'w');
+					$orderHandler = fopen(($orderDir . $verification . ".json"), "w");
 					fwrite($orderHandler, json_encode($orderObject));
 					fflush($orderHandler);
 					fclose($orderHandler);
